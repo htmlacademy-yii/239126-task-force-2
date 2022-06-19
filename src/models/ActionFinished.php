c0e3ae46bee1ef37d0e12a39025502ed53e57444
spook-work <hersgori4@protonmail.com>
2022-06-19 21:51:27 +0300

    finished ActionFinished.php

---
+++ src/models/ActionFinished.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TaskForce\models;
+
+use TaskForce\models\AbstractAction;
+
+class ActionFinished extends AbstractAction
+{
+    /**
+     * Возвращает название действия
+     * @return string
+     */
+    public static function getActionName(): string
+    {
+        return "Выполнено";
+    }
+
+    /**
+     * Возвращает статус код действия
+     * @return int
+     */
+    public static function getActionStatus(): int
+    {
+        return 4;
+    }
+
+    /**
+     * Метод проверки прав
+     * Возвращает true в случае если текущий пользователь
+     * является заказчиком задания
+     * @param int $executorId -- id исполнителя задания
+     * @param int $customerId -- id заказчика задания
+     * @param int $activeId -- id текущего пользователя
+     * @return bool
+     */
+    public static function checkPermission(int $executorId, int $customerId, int $activeId): bool
+    {
+        return $customerId === $activeId;
+    }
+}
