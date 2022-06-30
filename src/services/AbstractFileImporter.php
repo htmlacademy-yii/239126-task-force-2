92bead85d6b10c097a1fcf347a0268c96fa9d68d
spook-work <hersgori4@protonmail.com>
2022-07-01 02:34:42 +0300

    wrote php doc for AbstractFileImporter

--- src/services/AbstractFileImporter.php
+++ src/services/AbstractFileImporter.php
@@ -15,7 +15,7 @@ abstract class AbstractFileImporter
     private array $columns;
     private SplFileObject $fileObject;
 
-    private array $result = [];
+    private ?array $result = [];
 
     /**
      * AbstractFileImporter constructor.
@@ -28,9 +28,15 @@ abstract class AbstractFileImporter
         $this->columns = $columns;
     }
 
-    public function import(): void
+    /**
+     * Импортиурет данный из csv файла в массив.
+     * @return array<string>|null
+     *
+     * @throws FileFormatException|SourceFileException
+     */
+    public function import(): ?array
     {
-        if ($this->validateColumns($this->columns)) {
+        if (!$this->validateColumns($this->columns)) {
             throw new FileFormatException("Заданы неверно загаловки столбцов");
         }
 
@@ -53,14 +59,27 @@ abstract class AbstractFileImporter
         foreach ($this->getNextLIne() as $line) {
             $this->result[] = $line;
         }
+
+        return $this->result;
     }
 
+    /**
+     * Возвращает данные о заголовках в csv файле
+     * @return array<string>|null
+     */
     public function getHeaderData(): ?array
     {
         $this->fileObject->rewind();
         return $this->fileObject->fgetcsv();
     }
 
+    /**
+     * Итерируется по csv файлу и возвращает
+     * порцию данных из таблицы
+     * @yield array<string>
+     *
+     * @return null
+     */
     private function getNextLIne(): ?iterable
     {
         while (!$this->fileObject->eof()) {
@@ -70,6 +89,13 @@ abstract class AbstractFileImporter
         return null;
     }
 
+    /**
+     * Валидирует колонки csv файла
+     * порцию данных из таблицы
+     * @param array $columns
+     *
+     * @return bool
+     */
     private function validateColumns(array $columns): bool
     {
         if (count($columns)) {
