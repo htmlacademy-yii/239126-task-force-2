60001b0b918ca974c5c2ea091168452b25208c52
spook-work <hersgori4@protonmail.com>
2022-07-02 19:42:00 +0300

    added getPreparedStmt method

--- src/connectors/Database.php
+++ src/connectors/Database.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace TaskForce\connectors;
 
 use mysqli;
+use mysqli_stmt;
 use TaskForce\exceptions\DatabaseConnectException;
+use TaskForce\exceptions\DatabaseStmtException;
 
 abstract class Database
 {
@@ -28,5 +30,60 @@ abstract class Database
         mysqli_set_charset($con, "utf8");
 
         return $con;
+    }
+
+    /**
+     * Создает подготовленное выражение на
+     * основе готового SQL запроса и переданных данных
+     *
+     * @param mysqli $con Ресурс соединения
+     * @param string $sql SQL запрос с плейсхолдерами вместо значений
+     * @param array $data Данные для вставки на место плейсхолдеров
+     *
+     * @return mysqli_stmt Подготовленное выражение
+     * @throws DatabaseStmtException при работе с подготовленным выражением
+     */
+    public static function getPreparedStmt(mysqli $con, string $sql, array $data): mysqli_stmt
+    {
+        $stmt = mysqli_prepare($con, $sql);
+
+        if ($stmt === false) {
+            $errorMsg = 'Не удалось инициализировать'
+                . 'подготовленное выражение: ' . mysqli_error($con);
+            throw new DatabaseStmtException($errorMsg);
+        }
+
+        if ($data) {
+            $types = '';
+            $stmtData = [];
+
+            foreach ($data as $value) {
+                $type = 's';
+
+                if (is_int($value)) {
+                    $type = 'i';
+                } elseif (is_string($value)) {
+                    $type = 's';
+                } elseif (is_double($value)) {
+                    $type = 'd';
+                }
+
+                $types .= $type;
+                $stmtData[] = $value;
+            }
+
+            $values = array_merge([$stmt, $types], $stmtData);
+
+            $func = 'mysqli_stmt_bind_param';
+            $func(...$values);
+
+            if (mysqli_errno($con) > 0) {
+                $errorMsg = 'Не удалось связать подготовленное'
+                    . 'выражение с параметрами: ' . mysqli_error($con);
+                throw new DatabaseStmtException($errorMsg);
+            }
+        }
+
+        return $stmt;
     }
 }
