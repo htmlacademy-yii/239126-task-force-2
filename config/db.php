6515dcf594b8088cb9ec4a0fa17d5a3b3fcafba3
spook-work <hersgori4@protonmail.com>
2022-08-11 19:33:50 +0300

    added connection to database

--- config/db.php
+++ config/db.php
@@ -1,10 +1,20 @@
 <?php
 
+$con = require __DIR__ . "/../config.php";
+
+
+$db = $con["db"];
+
+$hostname = $db["host"];
+$dbName = $db["database"];
+$userName = $db["user"];
+$password = $db["password"];
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "mysql:host=" . $hostname  . ";dbname=" . $dbName,
+    'username' => $userName,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
