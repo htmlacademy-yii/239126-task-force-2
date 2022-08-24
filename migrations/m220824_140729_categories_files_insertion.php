52a175c400e9ad0eef714922b6c08aee54d7af48
spook-work <hersgori4@protonmail.com>
2022-08-24 17:08:00 +0300

    created categories_files_insertion migration

---
+++ migrations/m220824_140729_categories_files_insertion.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220824_140729_categories_files_insertion
+ */
+class m220824_140729_categories_files_insertion extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220824_140729_categories_files_insertion cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220824_140729_categories_files_insertion cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
