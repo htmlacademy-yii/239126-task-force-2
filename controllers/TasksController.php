575e500705cf3b8cac9dd2ea23729bd45017a9e8
spook-work <hersgori4@protonmail.com>
2022-09-14 01:23:03 +0300

    calling categories from DB and instatiate SearchTaskForm object

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use app\models\Categories;
+use app\models\SearchTaskForm;
 use yii\web\Controller;
 use app\models\Tasks;
 use yii\data\Sort;
@@ -11,7 +13,9 @@ class TasksController extends Controller
     public function actionIndex()
     {
         $tasks = Tasks::find()->orderBy(["start_date" => SORT_DESC])->all();
+        $searchTaskFormModel = new SearchTaskForm();
+        $categories = Categories::find()->select("name")->column();
 
-        return $this->render("index", compact("tasks"));
+        return $this->render("index", compact("tasks", "searchTaskFormModel", "categories"));
     }
 }
