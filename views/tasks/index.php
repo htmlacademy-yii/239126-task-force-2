1dfa30ef94412482ff85a48caaea3213f6d9b1da
spook-work <hersgori4@protonmail.com>
2022-09-14 21:13:27 +0300

    finished rendering of form

--- views/tasks/index.php
+++ views/tasks/index.php
@@ -1,60 +1,111 @@
 <?php
-/*foreach ($tasks as $task) {
-            print_r($task->id);
-            print_r("</br>");
-            print_r($task->name);
-            print_r("</br>");
-            print_r($task->description);
-            print_r("</br>");
-            print_r($task->category->name);
-            print_r("</br>");
-            print_r($task->city->name);
-            print_r("</br>");
-            print_r($task->price);
-            print_r("</br>");
-            print_r($task->start_date);
-            print_r("</br>");
-            print_r($task->expiration_date);
-            print_r("</br>");
-            print_r($task->customer->name);
-            print_r("</br>");
 
-            print_r("</br>");
-            print_r("</br>");
-        }
-*/
-?>
-
-<?php
 /**
- * @var $tasks array
+ * @var array $tasks
+ * @var SearchTaskForm $searchTaskFormModel
+ * @var array $categories
  */
+
+use app\models\SearchTaskForm;
+use yii\widgets\ActiveForm;
 
     $now = new DateTime();
 ?>
 
 <div class="left-column">
      <h3 class="head-main head-task">Новые задания</h3>
-    <?php foreach ($tasks as $task) : ?>
-     <div class="task-card">
-            <div class="header-task">
-                <a  href="#" class="link link--block link--big"><?= $task->name ?></a>
-                <p class="price price--task"><?= $task->price ?> ₽</p>
+    <?php if (count($tasks)) : ?>
+        <?php foreach ($tasks as $task) : ?>
+            <div class="task-card">
+                <div class="header-task">
+                    <a  href="#" class="link link--block link--big"><?= $task->name ?></a>
+                    <p class="price price--task"><?= $task->price ?> ₽</p>
+                </div>
+                <?php
+                    $taskDateTime = new DateTime($task->start_date);
+                    $diff = $taskDateTime->diff($now);
+                    $hours = $diff->h;
+                ?>
+                <p class="info-text"><span class="current-time"><?= $hours ?> часа </span>назад</p>
+                <p class="task-text">
+                    <?= $task->description ?>
+                </p>
+                <div class="footer-task">
+                    <p class="info-text town-text"><?= $task->city->name ?></p>
+                    <p class="info-text category-text"><?= $task->category->name ?></p>
+                    <a href="#" class="button button--black">Смотреть Задание</a>
+                </div>
             </div>
-            <?php
-                $taskDateTime = new DateTime($task->start_date);
-                $diff = $taskDateTime->diff($now);
-                $hours = $diff->h;
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p>Нет новых задач</p>
+    <?php endif; ?>
+</div>
+
+<div class="right-column">
+    <div class="right-card black">
+        <div class="search-form">
+            <?php $form = ActiveForm::begin(
+                ['method' => 'get', 'action' => ['']]
+            ); ?>
+           <h4 class="head-card">Категории</h4>
+           <?= $form
+               ->field(
+                   $searchTaskFormModel,
+                   "categories",
+                   [
+                       "template" => "{input}",
+                       "options" => ["class" => "form-group"],
+                   ]
+               )
+               ->checkboxList(
+                   $categories,
+                   [
+                       "class" => "checkbox-wrapper",
+                       "itemOptions" => [
+                               "labelOptions" => ["class" => "control-label"],
+                       ]
+                   ]
+               );
             ?>
-            <p class="info-text"><span class="current-time"><?= $hours ?> часа </span>назад</p>
-            <p class="task-text">
-                <?= $task->description ?>
-            </p>
-            <div class="footer-task">
-                <p class="info-text town-text"><?= $task->city->name ?></p>
-                <p class="info-text category-text"><?= $task->category->name ?></p>
-                <a href="#" class="button button--black">Смотреть Задание</a>
-            </div>
-     </div>
-    <?php endforeach; ?>
+
+            <h4 class="head-card">Дополнительно</h4>
+
+            <?= $form
+                ->field(
+                    $searchTaskFormModel,
+                    "withoutExecutor",
+                    [
+                        "template" => "{input}",
+                    ]
+                )
+                ->checkbox(
+                    [
+                        "labelOptions" => ["class" => "control-label"],
+                    ]
+                );
+            ?>
+
+            <?= $form
+                ->field(
+                    $searchTaskFormModel,
+                    "hoursInterval",
+                    [
+                        "template" => "{input}",
+                        "options" => ["class" => "form-group"],
+                    ]
+                )
+                ->dropDownList(
+                    [
+                        "1" => "1 час",
+                        "12" => "12 часов",
+                        "24" => "24 часа",
+                    ]
+                );
+            ?>
+
+            <input type="submit" class="button button--blue" value="Искать">
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
 </div>
